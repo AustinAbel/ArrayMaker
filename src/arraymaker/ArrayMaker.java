5fd9a02cc606725a3948f496af0819deadc48824
Austin Abel <aabel0785@stu.neisd.net>
2015-09-13 13:16:12 -0500

    Commit Two From Home Computer
    Additions: Comments And Formatting.

--- src/arraymaker/ArrayMaker.java
+++ src/arraymaker/ArrayMaker.java
@@ -3,10 +3,13 @@ package arraymaker;
 import java.util.Scanner;
 
 /******************************************************************************
- * TODO Comment
- * 
- * 
- * 
+ * Program Filename: 	ArrayMaker.java
+ * Author: 				Abel, Austin
+ * Date: 				September, 2015
+ * Description: 		The user will enter a number then the program will list
+ *  					number one to the number they chose.
+ * Input: 				The user's chosen number.
+ * Output: 				The list of numbers.
  * 
 ******************************************************************************/
 
@@ -19,15 +22,16 @@ public class ArrayMaker {
 	public static void main (String args[]) {
 		Scanner sc = new Scanner(System.in);
 		
-		/* TODO Comment */
+		/* Asks the user for their chosen number */
 		System.out.println("Pick a number.");
 			int InOne = sc.nextInt();
 		
-		// TODO Comment
+		// Creates an integer array with a size dependent on the user's input.
 		int[] ArrayOne = new int[InOne + 1];
 			
 			
-		/* TODO Comment */
+		/* Iterates through each "spot" in the array placing the
+		 *  corresponding number in it's correct spot. */
 		for (int i = 1; i < InOne + 1; i++) {
 
 			ArrayOne[i] = i;
@@ -36,6 +40,7 @@ public class ArrayMaker {
 			
 		}
 		
+		/* Prints each number in the array 'ArrayOne' */
 		for (int i = 1; i < ArrayOne.length; i++) {
 			
 			System.out.println(ArrayOne[i]);
